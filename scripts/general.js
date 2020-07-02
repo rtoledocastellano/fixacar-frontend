9e512123023465e89589d4acca5839d0b709e8ab
Fer Martin <fernandoMartinSantana@gmail.com>
2020-07-02 22:15:29 +0100

    Sorting working

--- scripts/general.js
+++ scripts/general.js
@@ -2,3 +2,9 @@ $(".hBack").on("click", function(e){
   e.preventDefault();
   window.history.back();
 });
+
+if (localStorage.getItem('name')) {
+  //alert(localStorage.getItem('name'))
+} else {
+  //alert('no estas logeado')
+}
