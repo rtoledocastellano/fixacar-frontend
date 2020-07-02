4bcb019004e708be46e9f729f1dac1bfb8eb1eb8
ratoledo <rtoledocastellano@gmail.com>
2020-07-02 23:24:46 +0100

    function to ReWrite Navbar with userName done

--- scripts/general.js
+++ scripts/general.js
@@ -1,10 +1,4 @@
-$(".hBack").on("click", function(e){
-  e.preventDefault();
-  window.history.back();
-});
-
-if (localStorage.getItem('name')) {
-  //alert(localStorage.getItem('name'))
-} else {
-  //alert('no estas logeado')
-}
+$('.hBack').on('click', function (e) {
+  e.preventDefault()
+  window.history.back()
+})
